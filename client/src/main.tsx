0a994640cf868fb3e674f3a534eb9dbc624aab1d
septiancahyocahyo <septiancahyo67@gmail.com>
2026-06-23 18:46:25 +0700

    feat: add route configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from '@/contexts/AuthContext'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* BrowserRouter: menghubungkan URL browser dengan routing React */}
+    <BrowserRouter>
+      {/* AuthProvider: menyediakan state autentikasi ke seluruh app */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
